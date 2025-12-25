fc60a863eea8cab99c432d45a1afcb90442fc371
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-12-26 00:16:03 +0600

    icons and logos

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" href="/public/favicon.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/public/favicon.png" />
         <title>Care.xyz - Trusted Baby Sitting & Elderly Care</title>
       </head>
       <body className="min-h-screen bg-background scroll-smooth">
